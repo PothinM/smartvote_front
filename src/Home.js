37e62394b6b3fd41ae5dc20317ecc038f872fced
PothinM <mathieu_pothin@hotmail.com>
2023-04-14 19:34:47 +0200

    V1.4

--- src/Home.js
+++ src/Home.js
@@ -2,8 +2,12 @@ import React from 'react';
 import { 
   Text,
   Box,
+  Image,
+  Center
 } from '@chakra-ui/react'
-  
+
+import logo from './logoSmartVote.png';
+
 const Home = () => {
   return (
     <div>
@@ -11,6 +15,9 @@ const Home = () => {
             <Text fontSize="5xl" fontWeight="bold" textAlign="center" mt="10px" mb="3px">
                     Accueil
             </Text>
+            <Center>
+              <Image src={logo}></Image>
+            </Center>
             <Text fontSize="3xl" textAlign="center" mt="10px" mb="3px">
                     Bienvenue sur Smart Vote, l'application de vote démocratique en ligne.
             </Text>
